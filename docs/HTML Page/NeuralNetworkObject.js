99473e3b65223f19067619200873b45b7ae4886b
DeD2aI80 <ded2ai80@gmail.com>
2016-12-03 18:16:28 +0100

    Neural Structure with a unique list

--- docs/HTML Page/NeuralNetworkObject.js
+++ docs/HTML Page/NeuralNetworkObject.js
@@ -13,9 +13,9 @@ function Neuron(nInputs) {
     this.output = this.sum;
     return this.sum;
   }
-  
+
   this.getAnswer = function() { return this.sum; }
-  
+
 }
 
 
@@ -30,23 +30,23 @@ function Layer(nNeurons, nInputs) {
     }
     return outputs;
   }
-  
+
   this.nNeurons = function(){ return this.neurons.length; }
-  
+
   this.getAnswer = function(n){ return this.neurons[n].getAnswer(); }
 
 }
 
 
-function Network(i, n, o) {
+function Network(layers) {
   this.inputs = [];
 
   this.layers = [];
-  this.layers.push(new Layer(n[0], i));   //Add 1st layer with inputs
-  for (var layer = 1; layer < n.length; layer++) {
-    this.layers.push(new Layer(n[layer], n[layer - 1]));
+  //this.layers.push(new Layer(n[0], i));   //Add 1st layer with inputs
+  for (var layer = 1; layer < layers.length; layer++) {
+    this.layers.push(new Layer(layers[layer], layers[layer - 1]));
   }
-  this.layers.push(new Layer(o, n[this.layers.length - 1]));  //Add output layer
+  //this.layers.push(new Layer(o, n[this.layers.length - 1]));  //Add output layer
 
 
   this.forward = function(inputs) {
@@ -57,31 +57,31 @@ function Network(i, n, o) {
     }
     return layerInputs;
   }
-  
-  
+
+
   this.display = function(w,h,rC){
-    
+
     w-=rC; h-=rC;
     translate(rC/2 + w/2, rC/2 + h/2);  //Enlève l'espace occupé par les rayons des cercles et centre au milieu
-    
+
     //Ajout des couches :
     var neuronsInLayer = [this.inputs.length-1]; //inputs.length-1};
-    
+
     for(var l=0; l<this.layers.length; l++) neuronsInLayer.push(this.layers[l].nNeurons()-1);
-    
+
     var e = h/max(neuronsInLayer);  //Ecart standard entre les cercles défini par la 'couche' ayant le plus de cercles
     var nLayers = neuronsInLayer.length;
-    
+
     //stroke(0); strokeWeight(1);
     textAlign(CENTER,CENTER); textSize(12);
-    
-    for(var layer=0; layer<nLayers; layer++){ 
+
+    for(var layer=0; layer<nLayers; layer++){
       var x = w/(nLayers-1)*(layer-(nLayers-1)/2);  //Position x de la couche
       var y0 = -e*neuronsInLayer[layer]/2;          //Position du 1er neurone en haut
       var  y1=0; if(layer<nLayers-1) y1 = -e*neuronsInLayer[layer+1]/2;    //Position y de la couche suivante
-      
+
       for(var neuron=0; neuron <= neuronsInLayer[layer]; neuron++){  //Neurones de la couche
-        
+
         //Lignes qui se relient à la couche suivante :
         if(layer<nLayers-1){  //La dernière couche n'a pas de couche suivante !
           for(var nextNeuron=0; nextNeuron <= neuronsInLayer[layer+1]; nextNeuron++){
@@ -89,26 +89,25 @@ function Network(i, n, o) {
             line(x,y0+e*neuron,x+w/(nLayers-1),y1+e*nextNeuron);
           }
         }
-        
+
         //Neurone :
         fill(255);
         ellipse(x,y0+e*neuron,rC,rC);
-        
+
         fill(0);  //Valeur du neurone
-        
+
         if(layer===0){
           if(neuron > this.inputs.length-1) text("e",x,y0+e*neuron);
            else text(int(this.inputs[neuron]),x,y0+e*neuron);
         }else text(this.layers[layer-1].getAnswer(neuron),x,y0+e*neuron);
-        
+
       }
-      
+
     }
-   
+
   }
 
 
 
 
 }
-
--- docs/HTML Page/sketch.js
+++ docs/HTML Page/sketch.js
@@ -5,14 +5,14 @@ function setup() {
 
   var canvas = createCanvas(250, 250);
   canvas.parent('p5-graph');
-  
+
   structureField = select("#neural-structure-field-value");
   structureField.value("2 3 1");
   structureField.input(newFieldNeuralStructure);
 
 
-  displayNewNetwork(2, [3], 1);
-  
+  displayNewNetwork([2,3,1]);
+
 }
 
 
@@ -24,32 +24,32 @@ function newFieldNeuralStructure(){
   for(var i=numbers.length-1; i>=0; i--){
     if(numbers[i]=="") numbers.splice(i,1); //Supprime les éléments vide
      else if(!int(numbers[i])){ //Vérfie si la valeur est bien un nombre (>0)
-            onlyNumbers=false;
-            break;
-          }else numbers[i]=int(numbers[i]);
+      onlyNumbers=false;
+      break;
+    }else numbers[i]=int(numbers[i]);
   }
-  
-  if(numbers.length<3 || !onlyNumbers){ //Message d'erreur
-    console.log("! : il faut minimum 3 chiffres (>0)");
+
+  if(numbers.length<2 || !onlyNumbers){ //Message d'erreur
+    console.log("! : il faut minimum 2 chiffres (>0)");
     return ;
   }
 
-  var i = numbers[0];
-  var o = numbers.pop();  //Supprime et récupère la dernière valeur
-  numbers.splice(0,1);  //Enlève la première valeur -> numbers ne contient plus la 1ère et dernière valeur
-  var n = numbers;
+  //var i = numbers[0];
+  //var o = numbers.pop();  //Supprime et récupère la dernière valeur
+  //numbers.splice(0,1);  //Enlève la première valeur -> numbers ne contient plus la 1ère et dernière valeur
+  //var n = numbers;
 
-  displayNewNetwork(i,n,o);
+  displayNewNetwork(numbers);
 
 
 }
 
-function displayNewNetwork(i,n,o){
+function displayNewNetwork(layers){
   background(250);
-  n = new Network(i, n, o);
+  n = new Network(layers);
 
   var inputs = [];  //Nombre d'inputs en fonction du nombre d'entrée
-  for(var j=0; j<i; j++) inputs.push(1);
+  for(var j=0; j<layers[0]; j++) inputs.push(1);
 
   n.forward(inputs);
   n.display(width,height,32);
@@ -57,9 +57,5 @@ function displayNewNetwork(i,n,o){
 
 function draw() {
 
-  
+
 }
-
-
-
-
